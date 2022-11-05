ac916681d5933cb699ca82ce984ab2a72bdbb068
Quinten98 <quinten.van.cappellen@student.ehb.be>
2022-11-05 16:29:23 +0100

    added a canvas an drew an orange

--- exercises/1-functions/1-0-pizza.js
+++ exercises/1-functions/1-0-pizza.js
@@ -1,1 +1,21 @@
 "use strict";
+
+window.onresize = drawOrange;
+drawOrange();
+
+function drawOrange(){
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+
+    context.beginPath();
+    context.fillStyle = "orange";
+    context.arc(100, 100, 50, 0, Math.PI * 2);
+    context.fill();
+    
+
+
+
+}
